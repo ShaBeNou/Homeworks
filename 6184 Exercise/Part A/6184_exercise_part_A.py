93dec0c9427051937fda85295ce2333807a02c42
Shahar <shahar.bennoun@mail.huji.ac.il>
2024-12-27 23:37:29 +0200

    setting up the option to run the .py file as a script

--- 6184 Exercise/Part A/6184_exercise_part_A.py
+++ 6184 Exercise/Part A/6184_exercise_part_A.py
@@ -1,9 +1,14 @@
 # Imports
+import sys
+import os
 import numpy as np
 import pandas as pd
 
-# Load the pkl file into a Dataframe
-data = pd.read_pickle('Python_data.pkl')
-
-import os
-print(os.getcwd())
+if hasattr(sys.modules['__main__'], '__file__'): # This 'if/else' statement allows the file to be run as a script
+                                                 # without manually editing the pkl file directory as long as both
+                                                 # the .pkl and .py file are in the same directory
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(script_dir, 'Python_data.pkl')
+    data = pd.read_pickle(file_path)
+else:
+    data = pd.read_pickle('C:/Users/97252/PycharmProjects/Homeworks/6184 Exercise/Part A/Python_data.pkl')
