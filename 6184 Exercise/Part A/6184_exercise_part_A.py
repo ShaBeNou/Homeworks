9b4106b42f1619b9d73f81867f669018606c6ea8
Shahar <shahar.bennoun@mail.huji.ac.il>
2024-12-28 22:11:06 +0200

    setting up the option to run the .py file as a standalone script. error handling

--- 6184 Exercise/Part A/6184_exercise_part_A.py
+++ 6184 Exercise/Part A/6184_exercise_part_A.py
@@ -4,22 +4,46 @@ import os
 import numpy as np
 import pandas as pd
 
-# A lot of code just to make sure the file can be run as a standalone script
-if hasattr(sys.modules['__main__'], '__file__'): # This 'if/else' statement allows the file to be run as a script
-                                                 # without manually editing the pkl file directory as long as both
-                                                 # the .pkl and .py file are in the same directory
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(script_dir, 'Python_data.pkl')
-    if os.path.exists(file_path):
-        data = pd.read_pickle(file_path)
-        print('data loaded')
+
+def load_data(hardcoded_path=None):
+    """
+    A function to load the .pkl data file.
+    if run as a standalone script assumes the .pkl file is in the same directory as this code!
+
+    hardcoded_path: (str) path to hardcoded data file, in case the code is run through IDE.
+    """
+
+    # Check if run through IDE:
+    if hasattr(sys.modules['__main__'], '__file__'):
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        file_path = os.path.join(script_dir, 'Python_data.pkl')
+        if os.path.exists(file_path):
+            data = pd.read_pickle(file_path)
+            print('data loaded')
+        else:
+            raise FileNotFoundError(
+                'When running this code as a standalone script data file must be named "Python_data.pkl" and be in '
+                'the same directory as this script!')
     else:
-        raise Exception('If running this code as a standalone script data file should be in the same directory as this script.')
-else:                                            # if the code is being run from within an IDE console, uses hardcoded
-                                                 # set directory
-    file_path = 'C:/Users/97252/PycharmProjects/Homeworks/6184 Exercise/Part A/Python_data.pkl'
-    if os.path.exists(file_path):
-        data = pd.read_pickle(file_path)
-    else:
-        raise Exception('File not found, please manually define the data file directory')
+        file_path = hardcoded_path
+        if os.path.exists(file_path):
+            data = pd.read_pickle(file_path)
+        else:
+            raise FileNotFoundError(
+                'File not found. Please manually define a valid path to the .pkl file in `hardcoded_path`.')
+
+    return data
+
+if __name__ == '__main__':
+    try:
+        data = load_data(hardcoded_path='/6184 Exercise/Python_data.pkl')
+    except FileNotFoundError as e:
+        print(e)
+
+else:
+    raise Exception('This code is not intended to be run as a module, '
+                    'please run as a standalone script or within an IDE')
+
+
+
 
